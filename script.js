cc7fec4f10806f07ba5c84e512d5c776b4a8accc
pskuthe@gmail.com <pskuthe@gmail.com>
2021-01-25 17:10:35 -0500

    Javascript lab1

---
+++ script.js
@@ -0,0 +1,38 @@
+"use strict";
+let myName = `Pallavi Kuthe`;
+let age = 48;
+let birthday = `May 30`;
+let detroitGC = false;
+let lifeEvents = [
+  `I was born in Pune,India `,
+  "I went to Pune University",
+  `I love to travel`,
+  "I am doing front end bootcamp",
+];
+if (detroitGC) {
+  console.log(
+    "My name is ${myName} and I am a student at Grand Circus in Detroit, Michigan.My "
+  );
+} else {
+  console.log(
+    `I am currently ${age} years old and my birthday is on ${birthday}.`
+  );
+}
+for (let i = 0; i < lifeEvents.length; i++) {
+  console.log(lifeEvents[i]);
+}
+let counter = 0;
+let randomNumber = Math.floor(Math.random() * 10);
+console.log(`${randomNumber}`);
+//while (true) {
+//let randomNumber = Math.floor(Math.random() * 10);
+//console.log("${randomNumber}");
+//if (randomNumber != 5) {
+//counter++;
+//console.log(`${randomNumer} is !==5`);
+//} else {
+//counter++;
+//console.log(`${randomNumer} is 5==5`);
+//}
+//break;
+//}
